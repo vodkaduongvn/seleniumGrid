7773ab70e095b14eccdeffc462c46675edfaa6b5
vodkaduongvn <vodka.duongvn@gmail.com>
2021-05-21 17:25:44 +0700

    add screenshot

--- SeleniumGrid/Test.cs
+++ SeleniumGrid/Test.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
@@ -10,16 +11,41 @@ namespace SeleniumGrid
 {
     public class Tests
     {
+        protected IWebDriver _driver;
+
+        public Tests()
+        {
+            _driver = new ChromeDriver();
+        }
 
         [Test]
-        public void Test1()
+        public void TestPass()
         {
-            var chromeOptions = new ChromeOptions();
-            //chromeOptions.BrowserVersion = "90";
-            //chromeOptions.PlatformName = "windows";
-            IWebDriver driver = new RemoteWebDriver(new Uri("http://192.168.1.2:4444/wd/hub"), chromeOptions);
-            driver.Navigate().GoToUrl("http://www.google.com");
-            driver.Quit();
+            var actual = 1;
+            var expect = 1;
+            _driver.Navigate().GoToUrl("http://www.google.com");
+            Assert.AreEqual(actual, expect);
+
         }
+        [Test]
+        public void TestFail()
+        {
+            var actual = 1;
+            var expect = 2;
+            _driver.Navigate().GoToUrl("http://www.google.com");
+            Assert.AreEqual(actual, expect);
+
+        }
+        [TearDown]
+        public void End()
+        {
+            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+            {
+                var screenshot = ((ITakesScreenshot)_driver).GetScreenshot();
+                screenshot.SaveAsFile("imageAsFileAttachment.jpg");
+            }
+            this._driver.Quit();
+        }
+
     }
 }
